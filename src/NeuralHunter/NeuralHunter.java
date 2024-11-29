54b3cd0f164daf5c23055c37fff8a094750c96d3
thnbi <renato.hnbi@gmail.com>
2024-11-29 17:21:08 -0300

    feat: get events to rewards system

--- src/NeuralHunter/NeuralHunter.java
+++ src/NeuralHunter/NeuralHunter.java
@@ -10,4 +10,36 @@ public class NeuralHunter extends AdvancedRobot{
             execute();
         }
     }
+
+/* eventos para o sistema de recompensas e aprendizado
+    public void onScannedRobot(ScannedRobotEvent e) {
+    }
+
+    public void onBulletHit(BulletHitEvent e) {
+    }
+
+    public void onBulletMissed(BulletMissedEvent e) {
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+    }
+
+    public void onHitWall(HitWallEvent e) {
+    }
+
+    public void onWin(WinEvent event) {
+    }
+
+    public void onDeath(DeathEvent event) {
+    }
+*/
+
+    // calcula a ação com maior valor Q para o estado atual
+    private int calcMaxQvalueAction(){
+        return 0;
+    }
+
+
+
+
 }
