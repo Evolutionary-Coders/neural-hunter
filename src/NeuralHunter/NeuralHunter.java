9892112783c7a92bd7390db3f04817026e0f29d2
NicolasArthurDev <Nicolasarthur@gmail.com>
2024-12-05 20:24:02 -0300

    feat: Definindo as cores da animação de cores do robo

--- src/NeuralHunter/NeuralHunter.java
+++ src/NeuralHunter/NeuralHunter.java
@@ -1,9 +1,38 @@
 package NeuralHunter;
-
+import java.awt.*;
 import robocode.*;
 
 public class NeuralHunter extends AdvancedRobot{
     public void run(){
+        // Set colors
+        Color rosio = new Color(122, 13, 50);
+
+        //Arco Iru
+        Color vermelho = new Color(255, 0, 0);
+        Color laranja = new Color(255, 165, 0);
+        Color amarelo = new Color(255, 255, 0);
+        Color verde = new Color(0, 255, 0);
+        Color AzulMarin = new Color(0, 0, 255);
+        Color Roxo = new Color(128, 0, 128);
+        Color Rosa = new Color(255, 0, 255);
+
+
+        setBodyColor(rosio);
+        setGunColor(rosio);
+        setRadarColor(rosio);
+        setBulletColor(rosio);
+        setScanColor(rosio);
+
+        while(true){
+            setBodyColor(rosio);
+            setGunColor(rosio);
+            setRadarColor(rosio);
+            setBulletColor(rosio);
+            setScanColor(rosio);
+        }
+
+
+
         while(true){
             setAhead(100);
             setTurnRight(360);
