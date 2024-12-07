8bfb617b523da1437cc6c1e8349fb0a9609269a5
thnbi <luis.r13@aluno.ifsc.edu.br>
2024-12-07 17:26:33 -0300

    feat: implements actions #8

---
+++ src/NeuralHunter/Actions.java
@@ -0,0 +1,10 @@
+package NeuralHunter;
+
+public class Actions {
+    public static final int numActions = 5;
+    public static final int AHEAD_LEFT = 0;
+    public static final int AHEAD_RIGHT = 1;
+    public static final int BACK_LEFT = 2;
+    public static final int BACK_RIGHT = 3;
+    public static final int FIRE = 4;
+}
