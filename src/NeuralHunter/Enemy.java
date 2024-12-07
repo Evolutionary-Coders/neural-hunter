0a7e4559319dd7977bc58f9e56be21b9f87482d1
thnbi <luis.r13@aluno.ifsc.edu.br>
2024-12-07 16:47:05 -0300

    feat: create enemy class

---
+++ src/NeuralHunter/Enemy.java
@@ -0,0 +1,27 @@
+package NeuralHunter;
+
+public class Enemy {
+    private double angle;
+    private double distance;
+
+    public Enemy(double angle, double distance) {
+        this.angle = angle;
+        this.distance = distance;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
