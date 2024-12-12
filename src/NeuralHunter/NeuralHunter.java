947e271384f25a8ca94bc7af09d3e87f6c749aab
NicolasArthurDev <Nicolasarthur1806@gmail.com>
2024-12-12 19:46:09 -0300

    Fix: Removendo while de cores que estava travando o código #13

--- src/NeuralHunter/NeuralHunter.java
+++ src/NeuralHunter/NeuralHunter.java
@@ -5,16 +5,7 @@ import robocode.*;
 public class NeuralHunter extends AdvancedRobot{
     public void run(){
         // Set colors
-        Color rosio = new Color(122, 13, 50);
-
-        //Arco Iru
-        Color vermelho = new Color(255, 0, 0);
-        Color laranja = new Color(255, 165, 0);
-        Color amarelo = new Color(255, 255, 0);
-        Color verde = new Color(0, 255, 0);
-        Color AzulMarin = new Color(0, 0, 255);
-        Color Roxo = new Color(128, 0, 128);
-        Color Rosa = new Color(255, 0, 255);
+        Color rosio = new Color(255, 162, 196);
 
 
         setBodyColor(rosio);
@@ -22,16 +13,6 @@ public class NeuralHunter extends AdvancedRobot{
         setRadarColor(rosio);
         setBulletColor(rosio);
         setScanColor(rosio);
-
-        while(true){
-            setBodyColor(rosio);
-            setGunColor(rosio);
-            setRadarColor(rosio);
-            setBulletColor(rosio);
-            setScanColor(rosio);
-        }
-
-
 
         while(true){
             setAhead(100);
